ed14fefbdd1b41c8a14e701f87cf6958bc05ce65
anmol dhanda <rk352714@gmail.com>
2024-05-06 00:59:52 +0530

    form validation before the change of new form

--- main.js
+++ main.js
@@ -7,22 +7,46 @@ const second_previousbtn = document.querySelector(".second_previousbtn");
 const first_nextbtn = document.querySelector(".first_nextbtn");
 const second_nextbtn = document.querySelector(".second_nextbtn");
 const submitbtn = document.querySelector(".submitbtn");
+const input_name = document.querySelector("input[name='name']");
 
+function isform_valid(form) {
+  const all_inputfields = form.querySelectorAll(".inputfields");
+  let is_valid = true;
+  all_inputfields.forEach((each_inputfield)=>{
+    if(each_inputfield.value.trim() === "") {
+      is_valid = false;
+      each_inputfield.classList.add("formerror");
+      console.log("form not valid");
+    }
+    else {
+      is_valid = true;
+      each_inputfield.classList.remove("formerror");
+      console.log("form is valid");
+    }
+  })
+  return is_valid;
+}
+
+// ============================ function to update the width of progress indication line ============================
 function update_progress_indicationline_width(width_percentage) {
   progress_indication_line.style.width = `calc(100% - ${width_percentage})`;
 }
 
-// ======================== change the form's as per the user's clicked event by the previous next btns ========================
+// ======================== if the input fields' values are not empty then change the form's as per the user's clicked event by the previous next btns otherwise stuck at the same form ========================
 first_nextbtn.onclick = function () {
-  form1.style.left = "-450px";
-  form2.style.left = "5px";
-  update_progress_indicationline_width("33%");
+  if(isform_valid(form1)) {
+    form1.style.left = "-450px";
+    form2.style.left = "5px";
+    update_progress_indicationline_width("33%");
+  }
 };
 
 second_nextbtn.onclick = function () {
-  form2.style.left = "-451px";
-  form3.style.left = "5px";
-  update_progress_indicationline_width("0%");
+  if(isform_valid(form2)) {
+    form2.style.left = "-451px";
+    form3.style.left = "5px";
+    update_progress_indicationline_width("0%");
+  }
 };
 
 first_previousbtn.onclick = function () {
@@ -36,3 +60,9 @@ second_previousbtn.onclick = function () {
   form3.style.left = "450px";
   update_progress_indicationline_width("33%");
 };
+
+submitbtn.onclick = function(e) {
+  if(isform_valid(form3)) {
+    alert(`thank you for contacting us knull will reach out to you soon ${input_name.value}`);
+  }
+}
